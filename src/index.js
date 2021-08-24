6199dd50e2949f8d192250eb73ce3ece35387c22
泰志1020 <70305895+Taishi1020@users.noreply.github.com>
2021-08-24 18:11:42 +0900

    未完了リストに値の追加と、削除ボタンと完了ボタンを作成 完了ボタンと削除ボタンに関しては、未だアラート出力のみ

--- src/index.js
+++ src/index.js
@@ -13,9 +13,27 @@ const onClickAdd = () => {
   const li = document.createElement("li"); //liテキストを生成
   li.innerText = inputText; //innnerTextの中に、テキストボックスから取得した変数（入力した内容）を設定することで liタグが作成される
 
+  //完了ボタンタグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //削除ボタン生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   //divタグの子要素に書く要素をｓ設定
   div.appendChild(li);
-  console.log(div);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
